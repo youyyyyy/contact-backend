b1ff1b378ac6668067e445f9f59eb2c5acbe646f
youyyyyy <you519900911@gmail.com>
2023-01-15 23:36:13 -0500

    add handler method for GET, POST, PUT, DELETE

---
+++ contacts/src/main/java/com/ltp/contacts/web/ContactController.java
@@ -0,0 +1,48 @@
+package com.ltp.contacts.web;
+
+import com.ltp.contacts.pojo.Contact;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.ltp.contacts.service.ContactService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ContactController {
+
+    @Autowired
+    private ContactService contactService;
+
+    @GetMapping("/contact/{id}")
+    public ResponseEntity<Contact> getContact(@PathVariable String id) {
+        Contact contact = contactService.getContactById(id);
+        return new ResponseEntity<>(contact, HttpStatus.OK);
+    }
+
+    @GetMapping("/contact/all")
+    public ResponseEntity<List<Contact>> getContacts() {
+        List<Contact> contacts = contactService.getContacts();
+        return new ResponseEntity<>(contacts, HttpStatus.OK);
+    }
+
+    @PostMapping("/contact")
+    public ResponseEntity<HttpStatus> createContact(@RequestBody Contact contact) {
+        contactService.saveContact(contact);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/contact/{id}")
+    public ResponseEntity<Contact> updateContact(@PathVariable String id, @RequestBody Contact contact) {
+        contactService.updateContact(id, contact);
+        return new ResponseEntity<>(contact, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/contact/{id}")
+    public ResponseEntity<HttpStatus> deleteContact(@PathVariable String id) {
+        contactService.deleteContact(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
